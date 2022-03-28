232da9bda53d67165b9de3ba7f91d8710319e447
HUMZA Tareen <humzakhawartareen@gmail.com>
2022-03-29 00:32:11 +0500

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima_weather_app/services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var status = await Permission.locationWhenInUse.request();
+    print(status);
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
