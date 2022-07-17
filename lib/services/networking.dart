0ef5a211699b4308cf44f01d20924b6f22ce81bc
Humza Tareen <77065466+humzakt@users.noreply.github.com>
2022-07-17 16:39:25 +0500

    Update networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,19 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class NetworkHelper{
+class NetworkHelper {
   NetworkHelper(this.url);
 
   final String url;
 
-  Future getData() async{
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
 
-    http.Response response= await http.get(Uri.parse(url));
-
-    if(response.statusCode==200){
-      String data=response.body;
+    if (response.statusCode == 200) {
+      String data = response.body;
       return jsonDecode(data);
-    }else{
+    } else {
       print(response.statusCode);
     }
   }
